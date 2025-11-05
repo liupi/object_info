7212338c2a65a36d8636414653d448fcb1ba2f83
Liudmila <milalindley@gmail.com>
2025-11-05 20:19:22 +0100

    feat: add comments

--- src/objinfo/Main.java
+++ src/objinfo/Main.java
@@ -2,15 +2,26 @@ package objinfo;
 
 public class Main {
     public static void main(String[] args) {
+        //create object of the Car class by using the constructor,
         Car car = new Car("Leaf", "Nissan", 2020, 30500, "White", 10);
-        System.out.println(car.toString());
+
+        //call and print what the toString () method
+        System.out.println(car);
+
+        //we call the sell() method
         car.sell(2);
+
+        //print all fields (using the previously declared „ methods of the object field)
         System.out.println("Car " + " {model: " + car.getModel() +
                 ", brand: " + car.getBrand() + ", year: " +
                 car.getYear() + ", price: " + car.getPrice() +
                 ", color: " + car.getColor() + ", quantity " + car.getQuantity() + "}");
+
+        //using the "set" methods change some fields of the object
         car.setColor("Red");
         car.setPrice(32500);
-        System.out.println(car.toString());
+
+        //call and print again toString() method to notice the changes
+        System.out.println(car);
     }
 }
